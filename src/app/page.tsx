8eb75c353d8275fdaf4841db3a9ce379dd34adea
kiko.devv <Kiko.devv@gmail.com>
2025-09-25 23:02:39 +0200

    added responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import { IoMdPersonAdd, IoMdTrash } from "react-icons/io";
+
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center">
+    <div className="flex justify-center sm:h-screen items-center">
       <section className="grid bg-gradient-to-r from-gray-900 to-neutral-900 w-full mx-20 rounded-2xl overflow-hidden border-2 border-white">
         <h1 className="text-center font-bold text-white/90">Här ska du räkna dina bowling pöengar</h1>
-        <div className="flex">
+        {/*--------- add user section ----------*/}
+        <section className="flex justify-between items-center p-2">
+          <button className="flex items-center gap-1 bg-gray-600 rounded-md px-3 py-1 text-white hover:bg-gray-500 cursor-pointer transition-all duration-300 group">
+            <IoMdPersonAdd className="group-hover:scale-120 group-hover:text-sky-400 transition-all duration-300" />
+            Lägg till användare
+          </button>
+
+          <button className="flex items-center gap-1 bg-gray-600 rounded-md px-3 py-1 text-white hover:bg-gray-500 cursor-pointer transition-all duration-300 group">
+            <IoMdTrash className="group-hover:scale-120 group-hover:text-red-500 transition-all duration-300" /> Tabort
+            listan
+          </button>
+        </section>
+        {/*--------- loop score container ----------*/}
+        <div className="grid sm:flex">
           {Array.from({ length: 10 }).map((_, i) => (
             <div className="grid w-full" key={i}>
               <p className="text-center border border-gray-400">{i + 1}</p>
